71a1f51770dfb7514a5217317c6a3e51b1f40142
hari <harisakshith20@gmail.com>
2019-12-28 15:52:27 +0530

    testmessage

---
+++ src/test/java/firstmvm/mavendemoTest.java
@@ -0,0 +1,22 @@
+package firstmvm;
+
+import org.testng.annotations.Test;
+
+public class mavendemoTest 
+{
+	
+	/*@Test
+	public void mavendemo1()
+	{
+		System.out.println("welcome to HARI ");
+	}*/
+	
+	@Test
+	public void mavendemo1()
+	{
+		System.out.println("welcome to HARI WORLD ");
+	}
+	
+	
+
+}
